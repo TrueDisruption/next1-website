a33100c8bba9b2f09568b4b089191a83072d83fd
next1io <jordan@next1.io>
2016-11-12 11:04:27 +0100

    navbar changes

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,25 @@ $('.roundBtn.blue').hover(function(){
 	$(".reportHolder.iot").removeClass('iothover');
 })
 
+$('.reportHolder.iot').hover(function(){
+	$(".roundBtn.blue").addClass('hover');
+}, function(){
+	$(".roundBtn.blue").removeClass('hover');
+})
+
 $('.roundBtn.green').hover(function(){
 	$(".reportHolder.ai").addClass('aihover');
 }, function(){
 	$(".reportHolder.ai").removeClass('aihover');
+})
+
+$('.reportHolder.ai').hover(function(){
+	$(".roundBtn.green").addClass('hover');
+}, function(){
+	$(".roundBtn.green").removeClass('hover');
+})
+
+$('.close').click(function(e){
+	e.preventDefault()
+	$('.blueDescriptionBlock').hide()
 })
